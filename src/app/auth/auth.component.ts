dc9fe194ac471bc87bd7e09df370c0ba81b7ef60
Warit Santaputra <waritsan@gmail.com>
2019-10-14 18:45:06 +0700

    Add login form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
+
   constructor() { }
 
   ngOnInit() { }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
 }
